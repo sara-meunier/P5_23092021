acfc2d230a999af12f4af912e6ca3d24394199ac
sara-meunier <sara.meunie@gmail.com>
2021-09-25 10:48:13 +0200

    debut page d'accueil

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log ("ceci est un test");
---
+++ js/index.js
@@ -0,0 +1,28 @@
+//test pour vérifier que le javascript est bien chargé
+console.log("hello world");
+
+const API_URL = "http://localhost:3000/api/teddies";
+
+
+
+
+
+
+//getProducts();
+
+//document.getElementById("description1").innerText="blabla, test dans la div description";
+
+//afficheListeProduit();
+
+//getProduct(2);
+
+/*document.getElementById("photo1").innerText= getProduct(2); NON*/
+
+//creerDivBoucle();
+
+let nounours = [];
+creerListProduit(nounours);
+console.log(nounours);
+
+//pourquoi ç amarche pas ?
+console.log(nounours [2]);
