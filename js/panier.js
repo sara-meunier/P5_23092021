48b2e425dd8c7ac3a05befe469066643fa392755
sara-meunier <sara.meunie@gmail.com>
2021-10-03 16:10:57 +0200

    remplacement des div du panier par un tableau bootstrap

--- js/panier.js
+++ js/panier.js
@@ -8,39 +8,50 @@ if (localStorage.getItem("panier") === null) { //si le panier est vide
     let panierVide = document.createElement('p');
     panierVide.innerText= " Votre panier est vide";
     divPanier.appendChild(panierVide);
-
 }
+
 else { //si le panier est plein
     console.log ("le panier contient des objets");
     console.log(localStorage.panier);
+
+    let tableau = document.getElementById("tablebody");
 
     let panierStorage = localStorage.getItem("panier");
     let panier = JSON.parse(panierStorage);
     for (let i in panier) {
         //on crée la div qui contient le produit
-        let divProduct = document.createElement('div');
-        divProduct.classList.add("row");
-        divPanier.appendChild(divProduct);
+        let ligne = document.createElement('tr');
+        tableau.appendChild(ligne);
+
+
+        let divProduct = document.createElement('th');
+        //divProduct.scope.add("row");
+        divProduct.innerText = i;
+        ligne.appendChild(divProduct);
+
         
         //on crée la div avec le nom du produit
-        let divProductName = document.createElement('div');
+        let divProductName = document.createElement('td');
         divProductName.innerText = panier[i].productPanierName;
-        divProductName.classList.add("col", "border", "m-2");
-        divProduct.appendChild(divProductName);
+        //divProductName.classList.add("col", "border", "m-2");
+        ligne.appendChild(divProductName);
         
         //on crée la div avec le prix du produit
-        let divProductPrice = document.createElement('div');
+        let divProductPrice = document.createElement('td');
         divProductPrice.innerText = panier[i].productPanierPrice/100;
-        divProductPrice.classList.add("col", "border", "m-2", "text-center");
-        divProduct.appendChild(divProductPrice);
+        //divProductPrice.classList.add("col", "border", "m-2", "text-center");
+        ligne.appendChild(divProductPrice);
 
         // on crée le bouton +
+        let divButtonAdd = document.createElement("td");
+        ligne.appendChild(divButtonAdd);
+
         let buttonAdd = document.createElement('button');
         buttonAdd.innerText = "+";
         buttonAdd.classList.add("btn","btn-primary", "col-2", "m-2", "text-center", "add" );
         buttonAdd.type = "button"; 
-       // buttonAdd.id = "add";
-        divProduct.appendChild(buttonAdd);
+        divButtonAdd.appendChild(buttonAdd);
+
         buttonAdd.addEventListener("click", () => {
             alert("click-add");
             panier[i].productPanierNumber += 1;
@@ -48,15 +59,19 @@ else { //si le panier est plein
             localStorage.removeItem("panier");// on remplace l'ancien panier par le nouveau
             let variableStorage = JSON.stringify(panier);
             localStorage.setItem("panier", variableStorage);
-
-        })
+        });
         
         //on crée le bouton -
+
+        let divButtonRemove = document.createElement("td");
+        ligne.appendChild(divButtonRemove);
+
         let buttonRemove = document.createElement('button');
-        buttonRemove.classList.add("btn","btn-primary", "col-2", "m-2", "text-center","remove" );
-        buttonRemove.type = "button";
         buttonRemove.innerText = "-";
-        divProduct.appendChild(buttonRemove);
+        buttonRemove.classList.add("btn","btn-primary", "col-2", "m-2", "text-center","remove" );
+        buttonRemove.type = "button";        
+        divButtonRemove.appendChild(buttonRemove);
+
         buttonRemove.addEventListener("click", () => {
             alert("click-add");
             panier[i].productPanierNumber -= 1;
@@ -64,14 +79,13 @@ else { //si le panier est plein
             localStorage.removeItem("panier");// on remplace l'ancien panier par le nouveau
             let variableStorage = JSON.stringify(panier);
             localStorage.setItem("panier", variableStorage);
-
-        })
+        });
 
         //on crée la div avec la quantité du produit
-        let divNumber = document.createElement('div');
-        divNumber.classList.add("col", "border", "m-2", "text-center", "divNumber");
+        let divNumber = document.createElement('td');
+        //divNumber.classList.add("col", "border", "m-2", "text-center", "divNumber");
         divNumber.innerText = panier[i].productPanierNumber;
-        divProduct.appendChild(divNumber);
+        ligne.appendChild(divNumber);
     };
 
     //on crée la div avec le total
@@ -85,4 +99,3 @@ else { //si le panier est plein
     divResume.classList.add("col", "border", "m-2",);
     divPanier.appendChild(divResume);
 }
-;
